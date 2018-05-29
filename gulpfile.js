30856022f8b18e489bc20fc8f94ca8b7d0861b79
Allan Moreno <heyallan@Allans-MacBook-Pro.local>
2018-05-29 00:02:17 -0600

    Update repo/package name

--- gulpfile.js
+++ gulpfile.js
@@ -26,11 +26,11 @@ gulp.task('css', function() {
   return gulp.src('src/less/bundle.less')
     .pipe(less())
     .pipe(autoprefix())
-    .pipe(rename(pkgData.name + '.css'))
+    .pipe(rename(pkgData.keyword + '.css'))
     .pipe(header(banner, { package: pkgData }))
     .pipe(gulp.dest('dist')) // <-- deliver expanded for dist
     .pipe(minify())
-    .pipe(rename(pkgData.name + '.min.css'))
+    .pipe(rename(pkgData.keyword + '.min.css'))
     .pipe(header(banner, { package: pkgData }))
     .pipe(gulp.dest('dist')) // <-- deliver compressed for dist
     .pipe(gulp.dest('docs')) // <-- deliver extra copy for docs
@@ -46,6 +46,6 @@ gulp.task('html', function() {
 })
 
 gulp.task('default', ['css', 'html'], function() {
-  gulp.watch('src/less/*', ['css']),
+  gulp.watch('src/less/*', ['css', 'html']),
   gulp.watch('src/docs/**/*', ['html'])
 })
